0ea1e48cf48d22bfc1e4bb000a37f2b03dfe20d2
larussverris <djlarus@gmail.com>
2023-07-15 22:14:07 +0000

    added button to add files, as well as functionality to display the uploaded files in the sidebar

--- src/routes/SideBar.svelte
+++ src/routes/SideBar.svelte
@@ -1,13 +1,11 @@
 <script>
     import SideBarFileItem from "./SideBarFileItem.svelte";
     import SideBarGroupItem from "./SideBarGroupItem.svelte";
+    import AddButton from "./AddButton.svelte";
 </script>
 
 <div class="flex flex-col">
-    <h1 class="pt-5 text-black">Groups</h1>
-    <div>
-        <SideBarGroupItem />
-    </div>
+    <AddButton/>
     <h1 class="pt-5 text-black">Files</h1>
     <div>
         <SideBarFileItem />
